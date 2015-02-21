a249f479eaaaf8dcf5cea92f6c3b376fd170c537
Alex Wallar <aw204@st-andrews.ac.uk>
2015-02-21 15:28:29 +0000

    Adding

--- lagent/osm2nx.py
+++ lagent/osm2nx.py
@@ -13,7 +13,7 @@ def make_weighted(G):
     G = next(nx.connected_component_subgraphs(G))
     # For each edge, put (geographical distance * -1) as its weight. Calculate the largest distance beteen nodes
     max_distance = 0
-    for u,v,d in G.edges(data=True):
+    for u, v, d in G.edges(data=True):
         u_point = geo.Point(G.node[u]['data'].lat, G.node[u]['data'].lon)
         v_point = geo.Point(G.node[v]['data'].lat, G.node[v]['data'].lon)
         closeness = -(distance(u_point,v_point).kilometers)
@@ -51,11 +51,11 @@ def simplify_by_degree(G, max_distance):
     return G_simple
 
 
-G = read_osm(download_osm(-2.8473, 56.3207, -2.7600, 56.3672))
-G, max_distance = make_weighted(G)
-G = simplify_by_degree(G,max_distance)
+def get_osm_graph():
+    pass
 
 
-
-
-
+if __name__ == "__main__":
+    G = read_osm(download_osm(-2.8473, 56.3207, -2.7600, 56.3672))
+    G, max_distance = make_weighted(G)
+    G = simplify_by_degree(G,max_distance)
